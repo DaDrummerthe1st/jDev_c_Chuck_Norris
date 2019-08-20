0d04c68e169872dd469598c4ba37a11369324f64
Joakim Reuterborg <joakim.reuterborg@gmail.com>
2019-08-20 07:05:10 +0200

    added top level menu, submenu and one menu page (ugly)

--- jDev_c_Chuck_Norris.php
+++ jDev_c_Chuck_Norris.php
@@ -1,7 +1,7 @@
 <?php
 /*
     * Plugin Name: jDev see about Chuck Norris Widget
-    * Plugin URI: https://example.com/jDev_c_Chuck_Norris/
+    * Plugin URI: https://api.github.com/repos/DaDrummerthe1st/jDev_c_Chuck_Norris
     * Description: Berättelser om Chuck Norris
     * Version: 1.0
     * Author: Joakim Reuterborg
@@ -18,6 +18,9 @@ In technical terms: a WordPress Widget is a PHP object that echoes string data t
 add_action('widgets_init', function(){ // In order for this to show up on the sidebar
         register_widget('jDev_c_Chuck_Norris'); // Creates the class we want to register
     });
+
+// dashboard menu handler
+add_action( 'admin_menu', 'jDevcChuckNorrisCreateMenu');
 
 class jDev_c_Chuck_Norris extends WP_Widget { // extends inherits another standard class thus inheriting its possibilities ie drag-n-drop
     
@@ -74,36 +77,63 @@ class jDev_c_Chuck_Norris extends WP_Widget { // extends inherits another standa
                 />
 
             </label>
-
-            <select>
-            <?php
-            $retrievedCat = jDevcChuckNorrisretrieveCategories();
-            $countRetrievedCat = count($retrievedCat);
-            // Read the list of categories available for display
-                for($i = 0; $i < $countRetrievedCat; $i++) {
-                    echo "<option value='$i'>";
-                    echo $retrievedCat[$i];
-                    echo "</option>";
-                }
-            
-            ?>
-            </select>
         </p>
         <?php
     }
 
 }
 
+function jDevcChuckNorrisCreateMenu() {
+    
+    /********************************
+     * create custom top level menu *
+     * ******************************/
+    
+     add_menu_page( 'Chuck Norris Settings', 'jDev - Chuck Norris', 'manage_options', 'jDev_c_Chuck_Norris', 'jDev_c_Chuck_Norris_settings_page');
+    // add_menu_page( $page_title:string, $menu_title:string, $capability:string, $menu_slug:string, $function:callable, $icon_url:string, $position:integer|null )
+
+    // create submenu items
+    // add_submenu_page( $parent_slug:string, $page_title:string, $menu_title:string, $capability:string, $menu_slug:string, $function:callable )
+    add_submenu_page( 'jDev_c_Chuck_Norris', 'General settings page', 'General Settings menu', 'manage_options', 'jDev_c_Chuck_Norris_settings', 'jDev_c_Chuck_Norris_Settings' );
+    add_submenu_page( 'jDev_c_Chuck_Norris', 'About page', 'About menu', 'manage_options', 'jDev_c_Chuck_Norris_about', 'jDev_c_Chuck_Norris_about_page' );
+    add_submenu_page( 'jDev_c_Chuck_Norris', 'Uninstall page', 'Uninstall menu', 'manage_options', 'jDev_c_Chuck_Norris_uninstall', 'jDev_c_Chuck_Norris_uninstall_page' );
+    
+    /********************************
+     * create custom submenu *
+     * ******************************/
+    
+    add_options_page( 'jDev Settings', 'jDev Settings Page', 'manage_options', 'jDev_more_settings', 'jDev_c_Chuck_Norris_Settings' );
+}
+
+function jDev_c_Chuck_Norris_Settings() {
+?>
+    <h1>Inställningar för Chuck Norris berättelserna</h1>
+    <p class="jDev_c_Chuck_Norris_Settings">
+    <h5>Välj kategori</h5>
+    <select>
+        <?php
+        $retrievedCat = jDevcChuckNorrisretrieveCategories();
+        $countRetrievedCat = count($retrievedCat);
+        // Read the list of categories available for display
+            for($i = 0; $i < $countRetrievedCat; $i++) {
+                echo "<option value='$i'>";
+                echo $retrievedCat[$i];
+                echo "</option>";
+            }
+        
+    ?></select>
+    </p><?php
+}
+
 function jDevcChuckNorrisretrieveCategories () {
     $jDevcChuckNorrisUrl = 'https://api.chucknorris.io/jokes/categories';
     
-    $jDevcChuckNorrisResult = file_get_contents($jDevcChuckNorrisUrl);
+    // $jDevcChuckNorrisResult = file_get_contents($jDevcChuckNorrisUrl);
     // Will dump a beauty json :3
-    $jDevcChuckNorrisChuckCategories = json_decode(file_get_contents($jDevcChuckNorrisUrl));
-    // $jDevcChuckNorrisChuckCategories = gettype($jDevOneThing) . $jDevOneThing[1];
-
+    $jDevcChuckNorrisCategories = json_decode(file_get_contents($jDevcChuckNorrisUrl));
+    // $jDevcChuckNorrisCategories = gettype($jDevOneThing) . $jDevOneThing[1];
     
-    return $jDevcChuckNorrisChuckCategories;
+    return $jDevcChuckNorrisCategories;
 }
 
 ?>
